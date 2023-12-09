821f952197a76cd202a84b0ca9411a34ac477d3e
oliviagao9 <oliviagao9@gmail.com>
2023-12-08 21:52:28 -0600

    completed about page component

--- phase-2-project-puppy-club/src/About.js
+++ phase-2-project-puppy-club/src/About.js
@@ -1,8 +1,9 @@
 function About() {
   return (
-    <div>
+    <div className="about">
+      <img src="./images/about.jpg" alt="about" />
       Welcome to Pawsome Puppies Club!
-      
+
       At Pawsome Puppies Club, we believe that every wagging tail and floppy ear deserves a community that understands the boundless joy and companionship that comes with our furry friends. We are more than just a club; we are a family of puppy enthusiasts, dedicated to celebrating the unique charm and unconditional love that puppies bring into our lives.
     </div>
   )
