55e2661bd5514100009a4390ea9a81be29bfdd5c
oliviagao9 <oliviagao9@gmail.com>
2023-12-08 22:00:37 -0600

    updated membersToDisplay to include search filter

--- phase-2-project-puppy-club/src/MemberPage.js
+++ phase-2-project-puppy-club/src/MemberPage.js
@@ -2,7 +2,9 @@ import MemberList from "./MemberList";
 import Search from "./Search";
 
 function MemberPage({ searchInput, onSearchInput, members }) {
-  const membersToDisplay = members
+  const membersToDisplay = members.filter((member) => 
+    member.name.toLowerCase().includes(searchInput.toLowerCase())
+  )
 
   return (
     <main>
