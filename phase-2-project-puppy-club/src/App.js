c6df1e14d9b063a531bae4d3b65ec741f3be57cd
oliviagao9 <oliviagao9@gmail.com>
2023-12-08 19:14:33 -0600

    set up member states

--- phase-2-project-puppy-club/src/App.js
+++ phase-2-project-puppy-club/src/App.js
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [member, setMembers] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/puppys")
+    .then((res) => res.json())
+    .then((member) => {
+      setMembers(member);
+    });
+  }, []);
+
+  console.log(member);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
