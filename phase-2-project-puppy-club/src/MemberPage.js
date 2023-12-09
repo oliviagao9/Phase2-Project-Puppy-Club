e3c0bad91a1364f2b65e88736c7eced1c2884344
oliviagao9 <oliviagao9@gmail.com>
2023-12-08 21:58:43 -0600

    added Search component to MemberPage and Pass down prop

--- phase-2-project-puppy-club/src/MemberPage.js
+++ phase-2-project-puppy-club/src/MemberPage.js
@@ -1,10 +1,12 @@
 import MemberList from "./MemberList";
+import Search from "./Search";
 
-function MemberPage({ members }) {
+function MemberPage({ searchInput, onSearchInput, members }) {
   const membersToDisplay = members
 
   return (
     <main>
+      <Search searchInput = {searchInput} onSearchInput = {onSearchInput} />
       <MemberList members = {membersToDisplay} />
     </main>
   )
