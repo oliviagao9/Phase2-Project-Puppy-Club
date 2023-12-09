fbcd619cb89245288c101c3b05571a5ee39a29eb
oliviagao9 <oliviagao9@gmail.com>
2023-12-08 21:29:10 -0600

    add member form component completed

---
+++ phase-2-project-puppy-club/src/NewMemberForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+function NewMemberForm({ onAddMember }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [breed, setBreed] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let newMember = { name, image, breed };
+
+    fetch("http://localhost:4000/puppys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/JSON",
+      },
+      body: JSON.stringify(newMember),
+    })
+      .then((res) => res.json())
+      .then((member) => {
+        onAddMember(member);
+        alert("the new member is added!")
+      });
+  }
+
+  return (
+    <div id="member-form">
+      <h2>New Member</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          placeholder="member name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="image"
+          value={image}
+          placeholder="Image URL"
+          onChange={(e) => setImage(e.target.value)}
+        />
+        <input
+          type="text"
+          name="breed"
+          value={breed}
+          placeholder="member breed"
+          onChange={(e) => setBreed(e.target.value)}
+        />
+        <button type="submit">Add Member</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewMemberForm;
